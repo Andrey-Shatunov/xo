fc2b8621d116f250c958e68e62be660b2cf1b612
Tapac174 <andreshatuno@gmail.com>
2017-03-27 16:47:34 +0500

    xo pre end

--- polls/views.py
+++ polls/views.py
@@ -2,26 +2,32 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import get_object_or_404,render
 from django.http import Http404
-from .models import Question
-
+from .models import Question,Steps
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     print(context)
-    return render(request, 'polls/index.html', context)
+    return render(request, 'polls/my.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-def my(request):
-    all_entries = Steps.objects.all()
-    return render(request, 'polls/my.html')
+def xo(request):
+    if len(request.GET) > 0:
+        if request.GET.__getitem__("id")=="NewGame":
+            print("NewGame")
+            steps = Steps.objects.all()
+            steps.delete()
+        else:
+            if(len(list(Steps.objects.all()))%2==0):
+                p=Steps(x=request.GET.__getitem__("id"),y=0,type=1)
+                p.save()
+            elif(len(list(Steps.objects.all()))%2==1):
+                p=Steps(x=request.GET.__getitem__("id"),y=0,type=0)
+                p.save()
+            
+    steps = Steps.objects.all()
+    entry_list = list(steps)
+    my_list=[2 for i in range(9)]
+    for i in steps:
+        my_list[i.x]=i.type
+    context = {'my_steps0': my_list,'my_steps': my_list[0:3],'my_steps1': my_list[3:6],'my_steps2': my_list[6:9]}
+    return render(request, 'polls/my.html',context)
